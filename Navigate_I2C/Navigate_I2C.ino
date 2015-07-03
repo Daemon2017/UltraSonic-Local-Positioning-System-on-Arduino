cc7d80a2997ab72a6c42d8d41c8ae4bf3e31afef
Dmitrij <Daemon2017@gmail.com>
2015-07-03 21:25:14 +0300

    Release 3D v0.3

--- Navigate_I2C/Navigate_I2C.ino
+++ Navigate_I2C/Navigate_I2C.ino
@@ -38,25 +38,20 @@ RECEIVE_DATA_STRUCTURE_Z data_z;
 #define sync_y 8
 #define sync_z 9
 
-float sonic;
-float distance_base;
-float z;
+float sonic, distance_base, coordinate, true_coord;
+float x, y, z;
 
 void setup() 
 {
-  Serial.begin(115200);
-  
+  Serial.begin(115200);  
   Wire.begin(8);
-  
+  Wire.onReceive(receive); 
   et_x.begin(details(data_x), &Wire);
   et_y.begin(details(data_y), &Wire);
   et_z.begin(details(data_z), &Wire);
-
-  Wire.onReceive(receive);
   
   pinMode(sputnic_T_1, OUTPUT); 
   pinMode(sputnic_E_1, INPUT); 
-
   pinMode(client_T, OUTPUT); 
   pinMode(client_E, INPUT); 
   
@@ -71,17 +66,34 @@ void loop()
 {        
   getTemp();
   getBase();
+  getCoordY();
   getCoordZ();  
   getCoordX();
-  getCoordY();
+  delay (1500);
 }
 
-void sendWave()
-{
+void sendWave(int number)
+{      
+  float sync;
+  switch (number)
+  {
+    case 1:
+    sync = sync_x;
+    break;
+    
+    case 2:
+    sync = sync_y;
+    break;
+    
+    case 3:
+    sync = sync_z;
+    break;
+  }     
+  delay(500);         
+  digitalWrite(sync, HIGH); 
   delayMicroseconds(10);         
-  digitalWrite(client_T, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(client_T, LOW);   
+  getPriem();
+  digitalWrite(sync, LOW); 
 }
 
 void led()
@@ -93,11 +105,11 @@ void led()
 
 void getTemp()
 {
-  float temperatureC = (( ( (analogRead(A1) * 5.0) / 1024.0) - 0.55) * 100);  
+  float temperatureC = ((((analogRead(A1) * 5.0) / 1024.0) - 0.55) * 100);  
   Serial.println(" "); 
   Serial.print("Degrees: ");
   Serial.print(temperatureC); 
-    
+  
   sonic = sqrt(1.4 * 287 * (temperatureC + 273.15));
   Serial.println(" "); 
   Serial.print("Sonic speed: ");
@@ -106,101 +118,106 @@ void getTemp()
 
 void getBase()
 {
-  digitalWrite(client_T, HIGH);
   digitalWrite(sputnic_T_1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(client_T, LOW);
+  getPriem();
   digitalWrite(sputnic_T_1, LOW);
   float time_base = pulseIn(sputnic_E_1, HIGH);  
   distance_base = time_base * sonic / 1000000;
-  
   Serial.println(" ");  
-  Serial.print("time_base: ");
-  Serial.print(time_base, 0);
-  Serial.println(" "); 
+}
+
+void getPriem()
+{
+  digitalWrite(client_T, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(client_T, LOW);
 }
 
 void getCoordX()
 {
-  delay(500);         
-  digitalWrite(sync_x, HIGH); 
-  sendWave();   
-  digitalWrite(sync_x, LOW);
-  
+  sendWave(1);    
   if(et_x.receiveData())
   {                 
-    Serial.print("time_x: ");
-    Serial.print(data_x.time_x, 0);
-    Serial.println(" ");  
-    
-    float quad_x = 0.44;     
-    float distance = data_x.time_x * sonic / 1000000;
-    float Sp = (quad_x + distance_base + distance) / 2;
-    float Ar = sqrt(Sp * (Sp - quad_x) * (Sp - distance_base) * (Sp - distance));
-    float x = Ar / (quad_x / 2);
-    float true_x = sqrt(pow(x, 2) - pow(z, 2));
-            
+    x = calc(1);
     Serial.print("Z: ");
-    Serial.print(true_x, 3);
-    Serial.println(" ");    
-    
+    Serial.print(x, 3);
+    Serial.println(" ");        
     led();
    }  
 }
 
 void getCoordY()
-{
-  delay(500);         
-  digitalWrite(sync_y, HIGH); 
-  sendWave();
-  digitalWrite(sync_y, LOW);
-  
+{ 
+  sendWave(2);  
   if(et_y.receiveData())
   {                 
-    Serial.print("time_y: ");
-    Serial.print(data_y.time_y, 0);
-    Serial.println(" ");  
-         
-    float quad_y = 0.44;  
-    float distance = data_y.time_y * sonic / 1000000;
-    float Sp = (quad_y + distance_base + distance) / 2;
-    float Ar = sqrt(Sp * (Sp - quad_y) * (Sp - distance_base) * (Sp - distance));
-    float y = Ar / (quad_y / 2);
-    float true_y = sqrt(pow(y, 2) - pow(z, 2));
-            
+    y = calc(2);
     Serial.print("X: ");
-    Serial.print(true_y, 3);
-    Serial.println(" ");  
-  
+    Serial.print(y, 3);
+    Serial.println(" "); 
+    trueCalc(1);
+    Serial.print("X_true: ");
+    Serial.print(true_coord, 3);
+    Serial.println(" ");    
     led();
    }      
 }
 
 void getCoordZ()
 {
-  delay(500);         
-  digitalWrite(sync_z, HIGH); 
-  sendWave(); 
-  digitalWrite(sync_z, LOW);
-  
+  sendWave(3);   
   if(et_z.receiveData())
   {                    
-    Serial.print("time_z: ");
-    Serial.print(data_z.time_z, 0);
-    Serial.println(" ");  
-         
-    float quad_z = 0.50;       
-    float distance = data_z.time_z * sonic / 1000000;
-    float Sp = (quad_z + distance_base + distance) / 2;
-    float Ar = sqrt(Sp * (Sp - quad_z) * (Sp - distance_base) * (Sp - distance));
-    z = Ar / (quad_z / 2);
-            
+    z = calc(3); 
     Serial.print("Y: ");
     Serial.print(z, 3);
-    Serial.println(" ");  
-  
+    Serial.println(" ");      
+    trueCalc(2);
+    Serial.print("Y_true: ");
+    Serial.print(true_coord, 3);
+    Serial.println(" ");   
     led();
    }     
+}
+
+float calc(int peremennaja)
+{
+  float data, quad = 0.45;
+  switch (peremennaja)
+  {
+    case 1:
+    data = data_x.time_x;
+    break;
+    
+    case 2:
+    data = data_y.time_y;
+    break;
+    
+    case 3:
+    data = data_z.time_z;
+    break;
+  }   
+  Serial.println(" ");    
+  float distance = data * sonic / 1000000;
+  float Sp = (quad + distance_base + distance) / 2;
+  float Ar = sqrt(Sp * (Sp - quad) * (Sp - distance_base) * (Sp - distance));
+  return coordinate = Ar / (quad / 2);
+}
+
+float trueCalc(int znachenie)
+{
+  float coordinata;
+  switch (znachenie)
+  {
+    case 1:
+    coordinata = y;
+    break;
+    
+    case 2:
+    coordinata = z;
+    break;
+  } 
+  return true_coord = sqrt(pow(coordinata, 2) - pow(x, 2));   
 }
 
 void receive(int numBytes) {}
